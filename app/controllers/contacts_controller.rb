2c2f78996d75f59805e7a647d668bef65a33de24
kei366 <kei.abc36@gmail.com>
2020-11-18 01:55:48 +0900

    Add flash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.contact_mail(@contact).deliver
+      flash[:sucess]="送信しました"
       redirect_to root_path
     else
       redirect_to root_path
