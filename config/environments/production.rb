8c26b69743c0bc80bdae8cb9c9a5da06fca2575d
kei366 <kei.abc36@gmail.com>
2020-11-18 10:29:13 +0900

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sheltered-shore-67267.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
